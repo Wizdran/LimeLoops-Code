e32ec093218e05b9c5225b8a74329f3364e7994f
Matt Malto <mattmalto02@gmail.com>
2017-11-06 16:55:48 -0800

    name
    name ye

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Holocodo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Holocodo.java
@@ -2,18 +2,30 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+
 
 /**
  * Created by slistudent on 11/1/17.
  */
 @TeleOp(name="Pushbot: Teleop POV", group="Pushbot")
 
-public class Holocodo extends LinearOpMode {
+public class HardwarePushbot extends LinearOpMode {
 
     /* Declare OpMode members. */
-    HardwarePushbot robot = new HardwarePushbot();   // Use a Pushbot's hardware
-    // could also use HardwarePushbotMatrix class.
+
+    HardwarePushbot robot = new HardwarePushbot();
+    // Use a Pushbot's hardware
+    // could also use HardwarePushbotMatrix class if wanted.
+
+    DcMotor frontLeft;
+    DcMotor frontRight;
+    DcMotor backLeft;
+    DcMotor backRight;
 
     @Override
     public void runOpMode() {
@@ -61,6 +73,8 @@ public class Holocodo extends LinearOpMode {
             BLvalue = Range.clip(BLvalue, -1, 1);
             BRvalue = Range.clip(BRvalue, -1, 1);
 
+
+
             robot.frontLeft.setPower(FLvalue);
             robot.frontRight.setPower(FRvalue);
             robot.backLeft.setPower(BLvalue);
@@ -82,6 +96,78 @@ public class Holocodo extends LinearOpMode {
 
     }
 
+    /**
+     * This is NOT an opmode.
+     *
+     * This class can be used to define all the specific hardware for a single robot.
+     * In this case that robot is a Pushbot.
+     * See PushbotTeleopTank_Iterative and others classes starting with "Pushbot" for usage examples.
+     *
+     * This hardware class assumes the following device names have been configured on the robot:
+     * Note:  All names are lower case and some have single spaces between words.
+     *
+     Motor channel:  Left  drive motor:        "left_drive"
+     Motor channel:  Right drive motor:        "right_drive"
+     Motor channel:  Manipulator drive motor:  "left_arm"
+     Servo channel:  Servo to open left claw:  "left_hand"
+     Servo channel:  Servo to open right claw: "right_hand"
+     */
+
+
+        public static class HardwarePushbot {
+
+        // Public OpMode members.
+
+            public DcMotor  frontLeft    = null;
+            public DcMotor  frontRight   = null;
+            public DcMotor  backLeft     = null;
+            public DcMotor  backRight    = null;
+
+       /* public static final double MID_SERVO     =  0.5 ;
+        public static final double ARM_UP_POWER    =  0.45 ;
+        public static final double ARM_DOWN_POWER  = -0.45 ;: */
+
+        /* local OpMode members. */
+        HardwareMap hwMap           =  null;
+        private ElapsedTime period  = new ElapsedTime();
+
+        /* Constructor */
+        public HardwarePushbot(){
+
+        }
+
+        /* Initialize standard Hardware interfaces */
+        public void init (HardwareMap ahwMap) {
+            // Save reference to Hardware map
+            hwMap = ahwMap;
+
+            // Define and Initialize Motors
+            frontLeft  = hwMap.get(DcMotor.class, "front_left");
+            frontRight = hwMap.get(DcMotor.class, "front_right");
+            backLeft  = hwMap.get(DcMotor.class, "back_left");
+            backRight = hwMap.get(DcMotor.class, "back_right");
+
+
+            frontLeft.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+            frontRight.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+            backRight.setDirection(DcMotor.Direction.FORWARD); // Set to REVERSE if using AndyMark motors
+            backLeft.setDirection(DcMotor.Direction.REVERSE);// Set to FORWARD if using AndyMark motors
+
+            // Set all motors to zero power
+            frontLeft.setPower(0);
+            frontRight.setPower(0);
+            backLeft.setPower(0);
+            backRight.setPower(0);
+
+            // Set all motors to run without encoders.
+            // May want to use RUN_USING_ENCODERS if encoders are installed.
+            frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        }
+     }
 }
 
 
